812a25c7896bc50f960cbf008e209b9c50bb3a59
detuo <detuo@git.shdata.com>
2020-03-17 09:54:06 +0800

    Initial commit

---
+++ src/main/java/com/huaian/portablecode/PortableCodeApplication.java
@@ -0,0 +1,13 @@
+package com.huaian.portablecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortableCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortableCodeApplication.class, args);
+    }
+
+}
